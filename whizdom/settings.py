68f744d86aed644d8f3560245ff592a4e22980d2
waqas <waqashamid722@gmail.com>
2018-12-03 19:53:12 +0530

    Added id to basemodel mixin

--- whizdom/settings.py
+++ whizdom/settings.py
@@ -138,3 +138,14 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.BasicAuthentication',
     ),
 }
+
+# Caching with Redis
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
