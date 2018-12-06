457014e5dd56a1be1f456eaff1bd5d3f45386754
waqas <waqashamid722@gmail.com>
2018-12-06 18:46:29 +0530

    Fixed uuid bug

--- bouncer/mixins.py
+++ bouncer/mixins.py
@@ -4,7 +4,7 @@ from uuid import uuid4
 class BaseModelMixin(models.Model):
     created = models.DateTimeField(auto_now_add=True, null=True)
     modified = models.DateTimeField(auto_now=True)
-    id = models.CharField(primary_key=True, default=str(uuid4()), editable=False, max_length=36, unique=True)
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False, max_length=36, unique=True)
 
     class Meta:
         abstract = True
